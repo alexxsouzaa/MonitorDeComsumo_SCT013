e446a2532d22b402586ae27a1c15e2e503e60067
Bruno Alex <alex2804y@gmail.com>
2024-01-25 13:43:09 -0300

    style: Alteração nos comentários

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,29 @@
 #include <U8g2lib.h>
 #include "xbm.cpp"
 
-// Defina o tipo de display e os pinos de conexão
-U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE); // Driver do display OLED DE 1.3 Polegadas
+// Definição do pino do sensor
+const int pinSensor = 34;
+
+// Driver do display OLED DE 1.3 Polegadas
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+
 int x = 0;
 int y = 0;
+
 int periodo = 25;
 long int timeAtual = 0;
 long int timePosterior = 0;
 
-const int pinSensor = 34;
-
 void setup()
 {
+  // Inicializa o serial monitor
   Serial.begin(9600);
-  // Inicialize o display
+
+  // Inicializa o display
   u8g2.begin();
-  u8g2.setBusClock(400000);
+  // Define o contraste do display
   u8g2.setContrast(128);
+  // Define o pino do sensor como entrada
   pinMode(pinSensor, INPUT);
 }
 
@@ -26,26 +32,24 @@ void loop()
 {
   timeAtual = millis();
 
-  // Limpe o display
   u8g2.firstPage();
   do
   {
     // Status Bar
     u8g2.drawBox(0, 0, 128, 10);
-
+    // Ícone de atenção
     u8g2.setFontMode(2);
     u8g2.setDrawColor(0);
     u8g2.setFont(u8g2_font_open_iconic_embedded_1x_t);
     u8g2.drawGlyph(119, 9, 0x0047);
-
+    // Hora
     u8g2.setFont(u8g2_font_spleen5x8_mf);
     u8g2.setCursor(1, 8);
     u8g2.print("14:29");
-
+    // Temperatura
     u8g2.setFont(u8g2_font_spleen5x8_mf);
     u8g2.setCursor((u8g2.getWidth() - 11) / 2, 8);
     u8g2.print("31 'C");
-
     // Indicador de Corrente
     u8g2.setDrawColor(1);
     u8g2.setFont(u8g2_font_spleen16x32_me);
@@ -53,17 +57,14 @@ void loop()
     u8g2.print("1.03");
     u8g2.setFont(u8g2_font_spleen6x12_mf);
     u8g2.print(" A");
-
     // Indicador de Tensão e potencia
     u8g2.setFont(u8g2_font_spleen6x12_mf);
     u8g2.setCursor(97, 23);
     u8g2.print("127 V");
     u8g2.setCursor(97, 35);
     u8g2.print("130 W");
-
     // Gráfico
     u8g2.drawXBM(0, 42, grafico_width, grafico_height, grafico);
-
     if (timeAtual - timePosterior >= periodo)
     {
       timePosterior = timeAtual;
